19252d447648106c8a5beb40d7ed5c318c067793
Luis <luis@lu15.dev>
2023-08-23 13:20:11 +0100

    fix: remove all uses of extensions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     `java-library`
     `maven-publish`
+    id("net.kyori.blossom") version "1.3.1"
 }
 
 group = "dev.lu15"
@@ -23,11 +24,15 @@ dependencies {
             branch = "master"
         }
     }
-    compileOnlyApi("com.github.hollow-cube:minestom-ce:74ca1041f3")
+    compileOnlyApi("com.github.hollow-cube:minestom-ce:5ba3d92d83")
     compileOnlyApi("org.jetbrains:annotations:24.0.1")
 
-    testImplementation("com.github.hollow-cube:minestom-ce:74ca1041f3")
+    testImplementation("com.github.hollow-cube:minestom-ce:5ba3d92d83")
     testImplementation("org.slf4j:slf4j-simple:2.0.7")
+}
+
+blossom {
+    replaceToken("@VERSION@", project.rootProject.version.toString())
 }
 
 publishing {
--- src/main/java/dev/lu15/spark/minestom/Spark.java
+++ src/main/java/dev/lu15/spark/minestom/Spark.java
@@ -8,12 +8,9 @@ import me.lucko.spark.common.SparkPlugin;
 import me.lucko.spark.common.command.sender.CommandSender;
 import me.lucko.spark.common.monitor.ping.PlayerPingProvider;
 import me.lucko.spark.common.platform.PlatformInfo;
-import me.lucko.spark.common.sampler.source.ClassSourceLookup;
 import me.lucko.spark.common.tick.TickHook;
 import me.lucko.spark.common.tick.TickReporter;
-import me.lucko.spark.minestom.MinestomClassSourceLookup;
 import me.lucko.spark.minestom.MinestomCommandSender;
-import me.lucko.spark.minestom.MinestomPlatformInfo;
 import me.lucko.spark.minestom.MinestomPlayerPingProvider;
 import me.lucko.spark.minestom.MinestomTickHook;
 import me.lucko.spark.minestom.MinestomTickReporter;
@@ -58,7 +55,7 @@ public final class Spark implements SparkPlugin {
 
     @Override
     public String getVersion() {
-        return "1.10-SNAPSHOT"; // TODO: gradle parsing
+        return "@VERSION@"; // replaced by Gradle
     }
 
     @Override
@@ -95,11 +92,6 @@ public final class Spark implements SparkPlugin {
     @Override
     public PlatformInfo getPlatformInfo() {
         return new MinestomPlatformInfo();
-    }
-
-    @Override
-    public ClassSourceLookup createClassSourceLookup() {
-        return new MinestomClassSourceLookup();
     }
 
     @Override
